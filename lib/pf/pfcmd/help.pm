2739f4244c3ca5a815a07073243eeeae3db7fee5
James Rouzier <jrouzier@inverse.ca>
2014-08-06 10:36:39 -0400

    Added new services redis & pfqueue

--- lib/pf/pfcmd/help.pm
+++ lib/pf/pfcmd/help.pm
@@ -162,8 +162,10 @@ Services managed by PacketFence:
   pfdhcplistener   | PF DHCP monitoring daemon
   pfdns            | DNS daemon
   pfmon            | PF ARP monitoring daemon
+  pfqueue          | PF queue handling daemon
   pfsetvlan        | PF VLAN isolation daemon
   radiusd          | FreeRADIUS daemon
+  redis            | Redis daemon
   snmptrapd        | SNMP trap receiver daemon
   snort            | Sourcefire Snort IDS
   suricata         | Suricata IDS
